a5a61299abe9f5d3407554edae5b9250447191f4
hungrytech <74886812+hungrytech@users.noreply.github.com>
2022-10-01 01:03:44 +0900

    root 모듈 의존성 변경 (#8)

--- app-push-api/build.gradle.kts
+++ app-push-api/build.gradle.kts
@@ -1,8 +1,68 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+plugins {
+    id("org.springframework.boot") version "2.7.3"
+    id("io.spring.dependency-management") version "1.0.13.RELEASE"
+    id("org.asciidoctor.convert") version "1.5.8"
+    id("io.freefair.lombok") version "6.5.1"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+group = "com.flab"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.kafka:spring-kafka")
 
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("io.rest-assured:rest-assured:4.5.1")
+
     testImplementation("org.springframework.amqp:spring-rabbit-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+val snippetsDir by extra { file("build/generated-snippets") }
+tasks {
+    test {
+        outputs.dir(snippetsDir)
+    }
+
+    asciidoctor {
+        inputs.dir(snippetsDir)
+        sourceDir("src/main/asciidoc")
+        dependsOn(test)
+    }
 }
