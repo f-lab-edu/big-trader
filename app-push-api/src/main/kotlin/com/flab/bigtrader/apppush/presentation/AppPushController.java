83a853f34e1065a2d6912a03b7d597a69073a862
hungrytech <xorals9448@gmail.com>
2022-10-15 20:57:53 +0900

    refactor: AppPush API http status code 200만 반환하도록 변경

--- app-push-api/src/main/kotlin/com/flab/bigtrader/apppush/presentation/AppPushController.java
+++ app-push-api/src/main/kotlin/com/flab/bigtrader/apppush/presentation/AppPushController.java
@@ -1,28 +1,19 @@
 package com.flab.bigtrader.apppush.presentation;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.flab.bigtrader.apppush.common.AppPushRequestCounter;
-import com.flab.bigtrader.apppush.presentation.dto.AppPushSendResponse;
-
-import lombok.RequiredArgsConstructor;
-
 @RequestMapping("/api/v1")
 @RestController
 @RequiredArgsConstructor
 public class AppPushController {
 
-	private final AppPushRequestCounter appPushRequestCounter;
-
-	@PostMapping("/app-push")
-	@ResponseStatus(HttpStatus.OK)
-	public AppPushSendResponse sendMessage() {
-		long count = appPushRequestCounter.increaseAndGet();
-
-		return new AppPushSendResponse(count);
-	}
+    @PostMapping("/app-push")
+    @ResponseStatus(HttpStatus.OK)
+    public void sendMessage() {
+    }
 }
