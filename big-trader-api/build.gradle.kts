c8acf6f7c2ac5f7b0604c60e68e7c451579f04ae
hungrytech <xorals9448@gmail.com>
2022-11-23 17:07:59 +0900

    feat: jasypt 추가

--- big-trader-api/build.gradle.kts
+++ big-trader-api/build.gradle.kts
@@ -44,12 +44,22 @@ dependencies {
     // AOP
     implementation("org.springframework.boot:spring-boot-starter-aop")
 
+    // jasypt
+    implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.4")
+
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("io.rest-assured:rest-assured:4.5.1")
 
     testImplementation("org.springframework.amqp:spring-rabbit-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
+}
+
+configure<SourceSetContainer> {
+    named("main") {
+        java.srcDir("src/main/kotlin")
+    }
 }
 
 tasks.withType<KotlinCompile> {
