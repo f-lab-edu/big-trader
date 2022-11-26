27344081783f31dc0c0149e734a11730aeb7fc31
sjhello <sjhello235@gmail.com>
2022-11-26 22:07:53 +0900

    feat: commons, jasypt-config, kafka-config 모듈 생성

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
 rootProject.name = "bigtrader"
 include("app-push-api")
 include("big-trader-api")
+include("commons")
+include("commons:jasypt-config")
+findProject(":commons:jasypt-config")?.name = "jasypt-config"
+include("commons:kafka-config")
+findProject(":commons:kafka-config")?.name = "kafka-config"
