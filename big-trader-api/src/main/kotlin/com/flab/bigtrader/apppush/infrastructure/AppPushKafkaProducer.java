edc7f358359acac174662ba13a1c30dd9bcb8a84
sjhello <sjhello235@gmail.com>
2022-11-26 22:13:40 +0900

    feat: kafka 설정 분리

--- big-trader-api/src/main/kotlin/com/flab/bigtrader/apppush/infrastructure/AppPushKafkaProducer.java
+++ big-trader-api/src/main/kotlin/com/flab/bigtrader/apppush/infrastructure/AppPushKafkaProducer.java
@@ -1,8 +1,9 @@
 package com.flab.bigtrader.apppush.infrastructure;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+
+import com.flab.bigtrader.KafkaProperties;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,19 +11,19 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class AppPushKafkaProducer {
 
-	private final String topic;
-
 	private final KafkaTemplate<String, Object> kafkaTemplate;
 
+	private final KafkaProperties kafkaProperties;
+
 	public AppPushKafkaProducer(
-		@Value("${app.push.topic}") String topic,
+		KafkaProperties kafkaProperties,
 		KafkaTemplate<String, Object> kafkaTemplate) {
-		this.topic = topic;
 		this.kafkaTemplate = kafkaTemplate;
+		this.kafkaProperties = kafkaProperties;
 	}
 
 	public void sendAppPush(AppPushSendEvent appPushSendEvent) {
 		log.info("kafka send message");
-		kafkaTemplate.send(topic, appPushSendEvent);
+		kafkaTemplate.send(kafkaProperties.getTopic(), appPushSendEvent);
 	}
 }
