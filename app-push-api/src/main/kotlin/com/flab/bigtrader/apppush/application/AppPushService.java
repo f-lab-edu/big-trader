7f2119de93636a0c1fae550808462e3c01ad8009
sjhello <sjhello235@gmail.com>
2022-11-28 18:47:05 +0900

    feat: Consumer 작성

--- app-push-api/src/main/kotlin/com/flab/bigtrader/apppush/application/AppPushService.java
+++ app-push-api/src/main/kotlin/com/flab/bigtrader/apppush/application/AppPushService.java
@@ -1,8 +1,8 @@
 package com.flab.bigtrader.apppush.application;
 
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.flab.bigtrader.apppush.infrastructure.AppPushKafkaConsumer;
 import com.flab.bigtrader.apppush.presentation.dto.AppPushSendRequest;
 
 import lombok.RequiredArgsConstructor;
@@ -13,8 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class AppPushService {
 
-	@KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+	private final AppPushKafkaConsumer appPushKafkaConsumer;
+
 	public void sendMessage(AppPushSendRequest appPushSendRequest) {
-		log.info("consumer send message {}", appPushSendRequest.getMessage());
+		appPushKafkaConsumer.sendMessage(appPushSendRequest);
 	}
 }
---
+++ app-push-api/src/main/kotlin/com/flab/bigtrader/apppush/presentation/KafkaConsumeController.java
@@ -0,0 +1,27 @@
+package com.flab.bigtrader.apppush.presentation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flab.bigtrader.apppush.application.AppPushService;
+import com.flab.bigtrader.apppush.presentation.dto.AppPushSendRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/api/v2")
+@RestController
+@RequiredArgsConstructor
+public class KafkaConsumeController {
+	
+	private final AppPushService appPushService;
+
+	@PostMapping("/app-push")
+	@ResponseStatus(HttpStatus.OK)
+	public void sendMessage(@RequestBody AppPushSendRequest appPushSendRequest) {
+		appPushService.sendMessage(appPushSendRequest);
+	}
+}
