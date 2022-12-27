c7d17d1704117a7b6dc08ad6e7b58567f5501547
sjhello <sjhello235@gmail.com>
2022-12-28 01:22:14 +0900

    feat: 체결 kafka 이벤트 리스너, 서비스 작성

---
+++ stock-exchange/src/main/java/com/flab/bigtrader/stockexchange/application/StockExchangeService.java
@@ -0,0 +1,12 @@
+package com.flab.bigtrader.stockexchange.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockExchangeService {
+	/*
+	 * TODO
+	 * Redis에 매수 or 매도 주문이 있는 경우 체결 진행
+	 * 매수 or 매도 주문이 없는 경우 체결을 진행하지 않고 Redis에 저장
+	 * */
+}
---
+++ stock-exchange/src/main/java/com/flab/bigtrader/stockexchange/presentation/StockExchangeListener.java
@@ -0,0 +1,22 @@
+package com.flab.bigtrader.stockexchange.presentation;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.flab.bigtrader.stockexchange.application.StockExchangeService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StockExchangeListener {
+
+	private final StockExchangeService stockExchangeService;
+	
+	@KafkaListener(topics = "${spring.kafka.topic}")
+	public void stockExchange(String stock) {
+		log.info("{}", stock);
+	}
+}
