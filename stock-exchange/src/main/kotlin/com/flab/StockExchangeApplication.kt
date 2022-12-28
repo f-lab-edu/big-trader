3a918513d5a1172c4328bbea78a807ebb4d37451
sjhello <sjhello235@gmail.com>
2022-12-28 12:15:00 +0900

    refactor: 프로퍼티 스캔 추가

--- stock-exchange/src/main/kotlin/com/flab/StockExchangeApplication.kt
+++ stock-exchange/src/main/kotlin/com/flab/StockExchangeApplication.kt
@@ -1,9 +1,11 @@
 package com.flab
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class StockExchangeApplication
 
 fun main(args: Array<String>) {
