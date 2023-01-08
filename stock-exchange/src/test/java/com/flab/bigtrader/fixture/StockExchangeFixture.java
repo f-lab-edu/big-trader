6cce6b25a49d667752357012fbc0e3797898fc5a
hungrytech <xorals9448@gmail.com>
2023-01-08 20:10:07 +0900

    refactor: 테스트 fixture 적용

---
+++ stock-exchange/src/test/java/com/flab/bigtrader/fixture/StockExchangeFixture.java
@@ -0,0 +1,20 @@
+package com.flab.bigtrader.fixture;
+
+public class StockExchangeFixture {
+	/**
+	 * Stock Name
+	 */
+	public static final String SAMSUNG_STOCK = "SAMSUNG";
+
+	/**
+	 * Stock Count
+	 */
+	public static final Long BUY_STOCK_COUNT = 3L;
+
+	public static final Long SELL_STOCK_COUNT = 5L;
+
+	/**
+	 * STOCK Price
+	 */
+	public static final Long SAMSUNG_STOCK_PRICE = 52000L;
+}
--- stock-exchange/src/test/java/com/flab/bigtrader/stockexchange/application/StockExchangeServiceTest.java
+++ stock-exchange/src/test/java/com/flab/bigtrader/stockexchange/application/StockExchangeServiceTest.java
@@ -1,12 +1,13 @@
 package com.flab.bigtrader.stockexchange.application;
 
+import static com.flab.bigtrader.fixture.StockExchangeFixture.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,13 +33,11 @@ class StockExchangeServiceTest extends IntegrationTest {
 	@DisplayName("매수요청일때_체결할 수 있는 매도요청 이벤트가 없을경우 체결에 실패하고 저장된다")
 	@Test
 	void 매수요청일때_체결할_수_있는_매도요청_이벤트가_없을경우_체결에_실패하고_저장된다() {
-		String stockName = "SANSUNG";
-
 		StockExchangeEvent buyStockExchangeEvent = new StockExchangeEvent(
 			createUniqueId(),
-			stockName,
-			52000L,
-			3L,
+			SAMSUNG_STOCK,
+			SAMSUNG_STOCK_PRICE,
+			BUY_STOCK_COUNT,
 			TradingType.BUY
 		);
 
@@ -53,15 +52,13 @@ class StockExchangeServiceTest extends IntegrationTest {
 	@DisplayName("매수요청일때 체결할 수 있는 매도요청 이벤트의 개수가 같을 경우 체결에 성공한다")
 	@Test
 	void 매수요청일때_체결할_수_있는_매도요청_이벤트의_개수가_같을_경우_체결에_성공한다() {
-		String stockName = "SANSUNG";
-		Long count = 3L;
-		매도_요청_저장(stockName, count);
+		매도_요청_저장(SAMSUNG_STOCK, BUY_STOCK_COUNT);
 
 		StockExchangeEvent buyStockExchangeEvent = new StockExchangeEvent(
 			createUniqueId(),
-			stockName,
-			52000L,
-			count,
+			SAMSUNG_STOCK,
+			SAMSUNG_STOCK_PRICE,
+			BUY_STOCK_COUNT,
 			TradingType.BUY
 		);
 
@@ -76,16 +73,13 @@ class StockExchangeServiceTest extends IntegrationTest {
 	@DisplayName("매수요청일때 체결할 수 있는 매도요청의 개수가 많을 경우 남은 개수의 매도요청은 저장된다")
 	@Test
 	void 매수요청일때_체결할_수_있는_매도요청의_개수가_많을_경우_남은_개수의_매도요청은_저장된다() {
-		String stockName = "SANSUNG";
-		Long buyCount = 3L;
-		Long sellCount = 5L;
-		StockExchangeEvent sellStockExchangeEvent = 매도_요청_저장(stockName, sellCount);
+		StockExchangeEvent sellStockExchangeEvent = 매도_요청_저장(SAMSUNG_STOCK, SELL_STOCK_COUNT);
 
 		StockExchangeEvent buyStockExchangeEvent = new StockExchangeEvent(
 			createUniqueId(),
-			stockName,
-			52000L,
-			buyCount,
+			SAMSUNG_STOCK,
+			SAMSUNG_STOCK_PRICE,
+			BUY_STOCK_COUNT,
 			TradingType.BUY
 		);
 
@@ -94,9 +88,9 @@ class StockExchangeServiceTest extends IntegrationTest {
 		Optional<StockExchangeEvent> stockEvent = stockExchangeRedis.findStockEvent(
 			sellStockExchangeEvent.generateKey());
 
-		Assertions.assertAll(
+		assertAll(
 			() -> assertThat(stockEvent.isPresent()).isTrue(),
-			() -> assertThat(stockEvent.get().getCount()).isEqualTo(sellCount - buyCount)
+			() -> assertThat(stockEvent.get().getCount()).isEqualTo(SELL_STOCK_COUNT - BUY_STOCK_COUNT)
 		);
 	}
 
